c59d04c84ad771f0b506601ff32ec172983158ee
ATOROYO <davidatoroyosika@gmail.com>
2025-01-29 19:42:31 +0300

    Updated the theme_provider

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.light;
 
+  ThemeProvider() {
+    _loadTheme();
+  }
+
   ThemeMode get themeMode => _themeMode;
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final theme = prefs.getString('theme');
+    _themeMode = theme == 'dark'
+        ? ThemeMode.dark
+        : theme == 'system'
+            ? ThemeMode.system
+            : ThemeMode.light;
+    notifyListeners();
+  }
+
+  Future<void> _saveTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('theme', _themeMode.toString().split('.')[1]);
+  }
+
+  void setTheme(ThemeMode mode) {
+    _themeMode = mode;
+    _saveTheme();
+    notifyListeners();
+  }
 
   void toggleTheme() {
     _themeMode =
         _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    _saveTheme();
+    notifyListeners();
+  }
+
+  void syncWithSystem() {
+    _themeMode = ThemeMode.system;
+    _saveTheme();
     notifyListeners();
   }
 }
